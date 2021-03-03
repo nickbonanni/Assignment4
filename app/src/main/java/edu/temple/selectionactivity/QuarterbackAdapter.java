52776f66982b5cb266e5c1c3ee8f3db552ea1a67
nickbonanni <nickbo514@yahoo.com>
2021-03-03 09:08:59 -0500

    Initial commit. Using same code base as Assignment 3.

---
+++ app/src/main/java/edu/temple/selectionactivity/QuarterbackAdapter.java
@@ -0,0 +1,84 @@
+package edu.temple.selectionactivity;
+
+import android.content.Context;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class QuarterbackAdapter extends BaseAdapter {
+
+    Context context;
+    ArrayList<String> names;
+    int[] images;
+
+    public QuarterbackAdapter(Context context, ArrayList<String> names, int[] images) {
+        this.context = context;
+        this.names = names;
+        this.images = images;
+    }
+
+    @Override
+    public int getCount() {
+        return names.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return names.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getDropDownView(int position, View convertView, ViewGroup parent) {
+
+        LinearLayout ddView = (LinearLayout) getView(position, convertView, parent);
+        ddView.setGravity(Gravity.NO_GRAVITY);
+
+        // Initializing image
+        ImageView qbImageView = new ImageView(context);
+        qbImageView.setImageResource(images[position]);
+
+        // Fixing size
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(250, 250);
+        qbImageView.setLayoutParams(params);
+
+        // Changing Text Padding
+        TextView qbTextView = (TextView) ddView.getChildAt(0);
+        qbTextView.setPadding(30,90,0,0);
+
+        ddView.addView(qbImageView, 0);
+
+        return ddView;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        // Initializing layout
+        LinearLayout linearLayout = new LinearLayout(context);
+        linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+        linearLayout.setPadding(10,10,10,10);
+        linearLayout.setGravity(Gravity.CENTER);
+
+        // Initializing text view
+        TextView qbTextView = new TextView(context);
+        qbTextView.setTextSize(22);
+        qbTextView.setText(names.get(position));
+
+        linearLayout.addView(qbTextView);
+
+        return linearLayout;
+
+    }
+
+}
---
+++ app/src/main/java/edu/temple/selectionactivity/SelectionActivity.java
@@ -0,0 +1,55 @@
+package edu.temple.selectionactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+
+public class SelectionActivity extends AppCompatActivity {
+
+    Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+
+        ArrayList<String> nameArray = new ArrayList<String>();
+        nameArray.add(getString(R.string.instructions));
+        nameArray.add(getString(R.string.brady));
+        nameArray.add(getString(R.string.brees));
+        nameArray.add(getString(R.string.mahomes));
+        nameArray.add(getString(R.string.rodgers));
+        nameArray.add(getString(R.string.watson));
+
+        int[] qbImageArray = new int[]{R.drawable.transparent_square, R.drawable.brady, R.drawable.brees,
+                R.drawable.mahomes, R.drawable.rodgers, R.drawable.watson};
+
+        QuarterbackAdapter adapter = new QuarterbackAdapter(this, nameArray, qbImageArray);
+
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+               // qbImage.setImageResource(qbImageArray[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Do nothing. This should never happen.
+            }
+
+        });
+
+    }
+}
