3d340ec701053a208ffa1d8018fe6d3c7c77fd59
nickbonanni <nickbo514@yahoo.com>
2021-03-09 12:25:33 -0500

    Adapter and GridView complete

--- app/src/main/java/edu/temple/Assignment5/QuarterbackAdapter.java
+++ app/src/main/java/edu/temple/Assignment5/QuarterbackAdapter.java
@@ -1,7 +1,7 @@
 package edu.temple.Assignment5;
 
 import android.content.Context;
-import android.view.Gravity;
+import android.graphics.Typeface;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -44,26 +44,25 @@ public class QuarterbackAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
 
         RelativeLayout relativeLayout = new RelativeLayout(context);
+        relativeLayout.setPadding(10,10,10,10);
 
         // Initializing text view
         TextView qbTextView = new TextView(context);
-        qbTextView.setTextSize(25);
+        qbTextView.setTextSize(23);
         qbTextView.setTextColor(WHITE);
         qbTextView.setText(names.get(position));
+        qbTextView.setTypeface(null, Typeface.BOLD);
 
         // Initializing image
         ImageView qbImageView = new ImageView(context);
         qbImageView.setImageResource(images[position]);
-
-        // Fixing size
         RelativeLayout.LayoutParams imageParams = new RelativeLayout.LayoutParams(600, 500);
         qbImageView.setLayoutParams(imageParams);
 
         // Adding center params
         RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
                 ViewGroup.LayoutParams.WRAP_CONTENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT
-        );
+                ViewGroup.LayoutParams.WRAP_CONTENT);
         lp.addRule(RelativeLayout.CENTER_IN_PARENT);
 
         relativeLayout.addView(qbImageView);
